798862f5158133e703fc77fcfc391032799fef8c
JUAN DAVID SAAVEDRA REYES <90642323+Juancho2406@users.noreply.github.com>
2025-02-23 23:55:10 -0500

    resolve update function in Rooms table, and add (seat, reserveredSeat)

--- src/controllers/roomController.ts
+++ src/controllers/roomController.ts
@@ -1,20 +1,22 @@
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 class RoomService {
   // Crear sala
   static async createRoom(req, res) {
-    const { name, capacity } = req.body;
+    const { name, capacity, seats, reservedSeats } = req.body;
     const id = `${Date.now()}`; // Generar un ID único para la sala
 
     const params = {
-      TableName: 'RoomsTable',
+      TableName: "RoomsTable",
       Item: {
         id: id,
-        name: name,
-        capacity: capacity,
-      },
+        name,
+        capacity,
+        seats,
+        reservedSeats
+      }
     };
 
     try {
@@ -22,14 +24,14 @@ class RoomService {
       res.status(201).json(params.Item); // Retornar la sala creada
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Error al crear la sala' });
+      res.status(500).json({ error: "Error al crear la sala" });
     }
   }
 
   // Obtener todas las salas
   static async getRooms(req, res) {
     const params = {
-      TableName: 'RoomsTable',
+      TableName: "RoomsTable"
     };
 
     try {
@@ -37,7 +39,7 @@ class RoomService {
       res.status(200).json(result.Items); // Retornar las salas obtenidas
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Error al obtener las salas' });
+      res.status(500).json({ error: "Error al obtener las salas" });
     }
   }
 
@@ -47,34 +49,36 @@ class RoomService {
 
     if (!name) {
       return res.status(400).json({
-        error: 'Falta el parámetro obligatorio: name',
+        error: "Falta el parámetro obligatorio: name"
       });
     }
 
     // Buscar la sala por nombre primero para obtener su ID
     const searchParams = {
-      TableName: 'RoomsTable',
-      FilterExpression: 'name = :name',
+      TableName: "RoomsTable",
+      FilterExpression: "name = :name",
       ExpressionAttributeValues: {
-        ':name': name,
-      },
+        ":name": name
+      }
     };
 
     try {
       const result = await dynamoDb.scan(searchParams).promise(); // Realizar el escaneo para encontrar la sala con ese nombre
 
       if (result.Items.length === 0) {
-        return res.status(404).json({ error: 'Sala no encontrada con ese nombre' });
+        return res
+          .status(404)
+          .json({ error: "Sala no encontrada con ese nombre" });
       }
 
       const roomToDelete = result.Items[0]; // Tomamos la primera coincidencia, asumimos que el nombre es único
 
       // Ahora eliminamos la sala por el ID
       const deleteParams = {
-        TableName: 'RoomsTable',
+        TableName: "RoomsTable",
         Key: {
-          id: roomToDelete.id, // Usamos el ID de la sala encontrada
-        },
+          id: roomToDelete.id // Usamos el ID de la sala encontrada
+        }
       };
 
       // Eliminar la sala
@@ -82,51 +86,53 @@ class RoomService {
 
       // Respuesta exitosa
       return res.status(200).json({
-        message: `Sala con nombre "${name}" eliminada correctamente`,
+        message: `Sala con nombre "${name}" eliminada correctamente`
       });
     } catch (error) {
-      console.error('Error al eliminar la sala:', error);
+      console.error("Error al eliminar la sala:", error);
       return res.status(500).json({
-        error: 'Error al eliminar la sala',
+        error: "Error al eliminar la sala"
       });
     }
   }
 
   // Actualizar una sala
   static async updateRoom(req, res) {
-    const { id, name, capacity } = req.body;
+    const { id, name, capacity, seats, reservedSeats } = req.body;
 
     // Validación de entradas
     if (!id || !name || !capacity) {
       return res.status(400).json({
-        error: 'Faltan parámetros obligatorios: id, name, capacity',
+        error: "Faltan parámetros obligatorios: id, name, capacity"
       });
     }
 
     const params = {
-      TableName: 'RoomsTable',
+      TableName: "RoomsTable",
       Key: {
-        id: id, // ID de la sala que queremos actualizar
+        id: id // ID de la sala que queremos actualizar
       },
-      UpdateExpression: 'set name = :name, capacity = :capacity',
+      UpdateExpression: "set name = :name, capacity = :capacity",
       ExpressionAttributeValues: {
-        ':name': name,
-        ':capacity': capacity,
+        ":name": name,
+        ":capacity": capacity,
+        ":seats": seats,
+        ":reservedSeats": reservedSeats
       },
-      ReturnValues: 'UPDATED_NEW', // Para retornar solo los valores actualizados
+      ReturnValues: "UPDATED_NEW" // Para retornar solo los valores actualizados
     };
 
     try {
       const result = await dynamoDb.update(params).promise();
 
       res.status(200).json({
-        message: 'Sala actualizada correctamente',
-        updatedRoom: result.Attributes, // Los valores actualizados
+        message: "Sala actualizada correctamente",
+        updatedRoom: result.Attributes // Los valores actualizados
       });
     } catch (error) {
-      console.error('Error al actualizar la sala:', error);
+      console.error("Error al actualizar la sala:", error);
       res.status(500).json({
-        error: 'Error al actualizar la sala',
+        error: "Error al actualizar la sala"
       });
     }
   }
